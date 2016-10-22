3b5d880975654539b3de573267760d34608e2d2e
Florian Pollak <fpdevelop@gmail.com>
2016-10-22 03:26:44 +0200

    updated copyright headers

--- src/main/java/com/github/bfour/fpliteraturecollector/domain/AtomicRequest.java
+++ src/main/java/com/github/bfour/fpliteraturecollector/domain/AtomicRequest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Florian Pollak
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.github.bfour.fpliteraturecollector.domain;
 
 import java.util.Date;
@@ -9,26 +25,6 @@ import org.springframework.data.neo4j.annotation.RelatedTo;
 import com.github.bfour.fpjpersist.neo4j.model.Neo4JEntity;
 import com.github.bfour.fpjsearch.fpjsearch.Searchable;
 import com.github.bfour.fpliteraturecollector.service.crawlers.Crawler;
-
-/*
- * -\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\-
- * FP-LiteratureCollector
- * =================================
- * Copyright (C) 2014 - 2015 Florian Pollak
- * =================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * -///////////////////////////////-
- */
 
 public class AtomicRequest extends Neo4JEntity implements Searchable {
 
--- src/main/java/org/epop/dataprovider/acmdigital/ACMDigitalLibrarySearch.java
+++ src/main/java/org/epop/dataprovider/acmdigital/ACMDigitalLibrarySearch.java
@@ -1,16 +1,15 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2012 fm&selab.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     fm&selab - initial API and implementation
- *     
  *     TESI DI Claudio Capelli c.capelli2@studenti.unibg.it
- *     
- ******************************************************************************/
+ *     Florian Pollak - several modifications
+ */
 package org.epop.dataprovider.acmdigital;
 
 import java.io.IOException;
